8a16ee0eca655fec94652cb5c800bdbf5d245948
manuelmelchiorri96 <manuelmelchiorri1996@gmail.com>
2023-12-03 08:55:18 +0100

    primo commit

---
+++ src/main/java/com/vivafattura/VivafatturaCreazioneAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vivafattura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivafatturaCreazioneAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivafatturaCreazioneAccountServiceApplication.class, args);
+	}
+
+}
